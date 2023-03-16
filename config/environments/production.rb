0fbabae917b76c74d769855b8f970beb5997a466
ShivaniChouhan <shchouhan@bestpeers.com>
2023-03-16 19:37:45 +0530

    complete commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.secret_key_base = Rails.application.credentials.secret_key_base
+
+
 end
