645842040f5aae002280f2d3afd3b8051096ebda
ShivaniChouhan <shchouhan@bestpeers.com>
2023-03-16 16:08:19 +0530

    complete commit

--- app/controllers/scope/registrations_controller.rb
+++ app/controllers/scope/registrations_controller.rb
@@ -93,21 +93,8 @@ class Scope::RegistrationsController < Devise::RegistrationsController
    # end
  end
 
- # DELETE /resource
- # def destroy
- #   super
- # end
 
- # GET /resource/cancel
- # Forces the session data which is usually expired after sign
- # in to be expired now. This is useful if the user wants to
- # cancel oauth signing in/up in the middle of the process,
- # removing all OAuth session data.
- # def cancel
- #   super
- # end
-
- # protected
+  #protected
 
  # If you have extra params to permit, append them to the sanitizer.
  def configure_sign_up_params
